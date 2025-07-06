88071cee18437fdca56ecc9642e49f0a5b6dc581
Snitchboss <ternux64@gmail.com>
2025-07-06 06:53:23 +0500

    Create EmeraldCollection.java

---
+++ src/main/java/me/carscupcake/sbremake/item/collections/impl/mining/EmeraldCollection.java
@@ -0,0 +1,61 @@
+package me.carscupcake.sbremake.item.collections.impl.mining;
+
+import me.carscupcake.sbremake.item.BaseSbItem;
+import me.carscupcake.sbremake.item.ISbItem;
+import me.carscupcake.sbremake.item.Recipe;
+import me.carscupcake.sbremake.item.collections.Collection;
+import me.carscupcake.sbremake.item.impl.other.mining.resources.EnchantedBlockOfCoal;
+import me.carscupcake.sbremake.item.impl.other.mining.resources.EnchantedCoal;
+import me.carscupcake.sbremake.player.SkyblockPlayer;
+import me.carscupcake.sbremake.rewards.impl.RecipeReward;
+import me.carscupcake.sbremake.rewards.impl.SkyblockXpReward;
+import net.minestom.server.item.Material;
+
+import java.util.List;
+
+public class CoalCollection extends Collection {
+    public CoalCollection(SkyblockPlayer player) {
+        super(player, new int[]{50, 100, 250, 1_000, 2_500, 5_000, 10_000, 25_000, 50_000}, List.of(List.of(new SkyblockXpReward(4)),
+                List.of(new SkyblockXpReward(4)), 
+                List.of(new SkyblockXpReward(4)), new RecipeReward(Recipe.craftingRecipes.get("COIN_TALISMAN")))
+                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("MAGNETIC_TALISMAN")))
+                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("ENCHANTED_EMERALD")))
+                List.of(new SkyblockXpReward(4)), new RecipeReward(Recipe.craftingRecipes.get("EMERALD_RING")))
+                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("PERSONAL_BANK_ITEM")))
+                List.of(new SkyblockXpReward(4)), new RecipeReward(Recipe.craftingRecipes.get("ENCHANTED_EMERALD_BLOCK")))
+                List.of(new SkyblockXpReward(4)), new RecipeReward(Recipe.craftingRecipes.get("EMERALD_BLADE")))
+                List.of(new SkyblockXpReward(4)))); new RecipeReward(Recipe.craftingRecipes.get("EMERALD_ARMOR_HELMET",
+                                                                                                "EMERALD_ARMOR_CHESTPLATE",
+                                                                                                "
+          
+                                                                                              
+    }
+
+    @Override
+    public Material showItem() {
+        return Material.COAL;
+    }
+
+    @Override
+    public String getId() {
+        return "COAL";
+    }
+
+    @Override
+    public String getName() {
+        return "Coal";
+    }
+
+    @Override
+    public int progress(ISbItem item) {
+        return switch (item) {
+            case BaseSbItem i -> {
+                if (i.getMaterial() == Material.COAL) yield 1;
+                yield 0;
+            }
+            case EnchantedCoal _ -> 160;
+            case EnchantedBlockOfCoal _ -> 160 * 160;
+            case null, default -> 0;
+        };
+    }
+                                                                                                             }
