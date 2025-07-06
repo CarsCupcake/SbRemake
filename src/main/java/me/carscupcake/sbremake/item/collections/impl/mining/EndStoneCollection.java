6f033575e94018dad07938049b950672b95679b9
Snitchboss <ternux64@gmail.com>
2025-07-06 08:01:44 +0500

    Update EndStoneCollection.java

--- src/main/java/me/carscupcake/sbremake/item/collections/impl/mining/EndStoneCollection.java
+++ src/main/java/me/carscupcake/sbremake/item/collections/impl/mining/EndStoneCollection.java
@@ -13,23 +13,21 @@ import net.minestom.server.item.Material;
 
 import java.util.List;
 
-public class EmeraldCollection extends Collection {
-    public EmeraldCollection(SkyblockPlayer player) {
+public class EndStoneCollection extends Collection {
+    public EndStoneCollection(SkyblockPlayer player) {
         super(player, new int[]{50, 100, 250, 1_000, 2_500, 5_000, 10_000, 25_000, 50_000}, List.of(List.of(new SkyblockXpReward(4)),
                 List.of(new SkyblockXpReward(4)), 
-                List.of(new SkyblockXpReward(4)), new RecipeReward(Recipe.craftingRecipes.get("COIN_TALISMAN"))),
-                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("MAGNETIC_TALISMAN"))),
-                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("ENCHANTED_EMERALD"))),
-                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("EMERALD_RING"))),
-                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("PERSONAL_BANK_ITEM"))),
-                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("ENCHANTED_EMERALD_BLOCK"))),
-                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("EMERALD_BLADE"))),
-        
-                List.of(new RecipeReward(Recipe.craftingRecipes.get(new RecipeReward(Recipe.craftingRecipes.get ("EMERALD_ARMOR_HELMET")),
-                                                                                                new RecipeReward(Recipe.craftingRecipes.get ("EMERALD_ARMOR_CHESTPLATE")),
-                                                                                                new RecipeReward(Recipe.craftingRecipes.get ("EMERALD_ARMOR_LEGGINGS")),
-                                                                                                new RecipeReward(Recipe.craftingRecipes.get ("EMERALD_ARMOR_BOOTS")),
-        SkyblockXpReward(4))));
+                List.of(new SkyblockXpReward(4)), new RecipeReward(Recipe.craftingRecipes.get("END_BIOME_STICK"))),
+                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("PORTAL_TO_THE_END"))),
+                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("ENCHANTED_ENDSTONE"))),
+                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("SILENCE_BLOCK"))),
+                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("HASTE_BLOCK"))),
+                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("ENDERMITE"))),
+                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("END_TRAVEL_SCROLL"))),
+                List.of(new SkyblockXpReward(4), new RecipeReward(Recipe.craftingRecipes.get("CATALYST"))),
+                List.of(new RecipeReward(Recipe.craftingRecipes.get("END_STONE_SWORD"))),
+                List.of(new SkyblockXpReward(4))));
+               
         
         
         
@@ -37,29 +35,28 @@ public class EmeraldCollection extends Collection {
 
     @Override
     public Material showItem() {
-        return Material.EMERALD;
+        return Material.END_STONE;
     }
 
     @Override
     public String getId() {
-        return "EMERALD";
+        return "ENDSTONE";
     }
 
     @Override
     public String getName() {
-        return "EMERALD";
+        return "ENDSTONE";
     }
 
     @Override
     public int progress(ISbItem item) {
         return switch (item) {
             case BaseSbItem i -> {
-                if (i.getMaterial() == Material.EMERALD) yield 1;
+                if (i.getMaterial() == Material.END_STONE) yield 1;
                 yield 0;
             }
-            case EnchantedEmerald _ -> 160;
-            case EnchantedBlockOfEmerald _ -> 160 * 160;
+            case EnchantedEndstone _ -> 160;
             case null, default -> 0;
         };
     }
-              }
+}
