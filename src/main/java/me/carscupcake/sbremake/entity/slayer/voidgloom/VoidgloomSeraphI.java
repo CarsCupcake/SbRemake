6ae6b537dc4d783e0413d318c5128b9d86e20e28
Snitchboss <ternux64@gmail.com>
2025-07-07 02:13:45 +0500

    Update VoidgloomSeraphI.java

--- src/main/java/me/carscupcake/sbremake/entity/slayer/voidgloom/VoidgloomSeraphI.java
+++ src/main/java/me/carscupcake/sbremake/entity/slayer/voidgloom/VoidgloomSeraphI.java
@@ -1,5 +1,6 @@
 package me.carscupcake.sbremake.entity.slayer.voidgloom;
 
+import net.minestom.server.item.Material;
 import me.carscupcake.sbremake.entity.SkyblockEntity;
 import net.minestom.server.entity.EntityType;
 
@@ -7,12 +8,24 @@ public class VoidgloomSeraph1 extends SkyblockEntity {
     public VoidgloomSeraph1() {
         super(EntityType.ENDERMAN);
     }
+    
+    @Overide
+    public void spawn() {
+        super.spawn();
+        add AiGroup(zombieAiGroup(this, true));
+    }
 
+    
     @Override
     public float getMaxHealth() {
         return 300_000;
     }
 
+    @Override
+    public float getDamage() {
+        return 1_200;
+    }
+    
     @Override
     public String getName() {
         return "Voidgloom Seraph I";
