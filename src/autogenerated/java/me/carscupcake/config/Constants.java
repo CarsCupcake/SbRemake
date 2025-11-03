35ee5605f7306489ffec047ffcda8f29473a0d09
CarsCupcake <84076092+CarsCupcake@users.noreply.github.com>
2025-11-03 22:33:23 +0100

    Added missing files

---
+++ src/autogenerated/java/me/carscupcake/config/Constants.java
@@ -0,0 +1,19 @@
+package me.carscupcake.config;
+
+import java.lang.String;
+
+public final class Constants {
+  /**
+   * The value of the config option Test
+   */
+  public static final String TEST = "Test";
+
+  public static final class SLAYER {
+    public static final class ENDERMAN {
+      /**
+       * The value of the config option Ability
+       */
+      public static final String ABILITY = "Test";
+    }
+  }
+}
